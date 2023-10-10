28e0f24b6066e27e40aa38c8efdcecd93e6cd521
ayckermann <aya.mf12@gmail.com>
2023-10-10 14:09:41 +0700

    final

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_aplikasi_biodata
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_aplikasi_biodata/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_aplikasi_biodata
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
